e070d0ade01a30d133a0396c3f48c0adf0c54a02
Leo Ferrari <leoantferrari@gmail.com>
2023-07-30 21:23:06 +0200

    Changed the game

---
+++ src/main/java/com/leoantsmith/elloml/ElloMlApplication.java
@@ -0,0 +1,13 @@
+package com.leoantsmith.elloml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElloMlApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ElloMlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/leoantsmith/elloml/controller/ExampleController.java
@@ -0,0 +1,16 @@
+package com.leoantsmith.elloml.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/example")
+    public String hello() {
+            return "Hello mate, the time now is " + new Date() + "\n";
+    }
+
+}
